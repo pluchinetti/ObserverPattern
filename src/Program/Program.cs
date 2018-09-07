44132bcba74916fe1d5a914a6eed8e72135597cf
pluchinetti <40801333+pluchinetti@users.noreply.github.com>
2018-09-07 20:15:47 -0300

    The last time I tried this the monkey didn't survive. Let's hope it works better this time.

---
+++ src/Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// ¡Programa para aprender el patrón Observer!
+/// Notar que cuando se desea crear una carpeta Program y otra Library, se utiliza dotnet new console -n Program y
+///  dotnet new classLib -n Library -f netcoreapp2.1. De lo contrario,
+/// </summary>
+namespace Observer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TemperatureReporter tempRep = new TemperatureReporter();
+            TemperatureMonitor tempMon = new TemperatureMonitor();
+
+            tempMon.Subscribe(tempRep);
+            tempMon.GetTemperature();
+        }
+    }
+}
